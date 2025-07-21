8063aa44a6033e7a823aec39ed44cfe5f6bcd217
Alejo2396 <alejomore2396@gmail.com>
2025-07-21 14:25:18 -0500

    Delete ArreglosFunciones directory

--- ArreglosFunciones/js/libros.js
+++
@@ -1,8 +0,0 @@
-export default class libro{
-    constructor(titulo, autor, editorial, idioma ){
-        this.titulo=titulo;
-        this.autor=autor;
-        this.editorial=editorial;
-        this.idioma=idioma
-    }
-}
