06281e0626fd9565c1d7cef3c4f833abe4abafcb
Alejo2396 <alejomore2396@gmail.com>
2025-07-21 14:25:41 -0500

    Delete Modulos directory

--- Modulos/Ejercicio 9/contacto.js
+++
@@ -1,9 +0,0 @@
-export default class Contacto {
-  constructor(identificacion, nombre, apellido, correo, celular) {
-    this.identificacion = identificacion;
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.correo = correo;
-    this.celular = celular;
-  }
-}
